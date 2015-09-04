6c5f1d80f4fad9572d9c26bf1a7332810fd5e5aa
onyzhin <o.nyzhin@gmail.com>
2015-09-04 13:10:56 +0300

    photographers page done

--- partials/photographers.php
+++ partials/photographers.php
@@ -1,3 +1,36 @@
 <div class="photographer">
-	<div class="photographer-header"></div>
+	<div class="container">
+		<div class="photographer-header">
+			<div class="photographer-filter">
+				<ul>
+					<li><a href="#">videos</a></li>
+					<li><a href="#">editorial</a></li>
+					<li><a href="#">advertorial</a></li>
+					<li><a href="#">campaign</a></li>
+					<li><a href="#">portraits</a></li>
+					<li><a href="#">architecture</a></li>
+					<li><a href="#">all</a></li>
+				</ul>
+			
+			</div>
+			<div class="photographer-search zNice">
+				<form>
+					<input name="search" placeholder="search photographer" />
+				</form>
+			</div>
+		</div>
+		<div class="photographer-body">
+			<div class="photographer-list">
+				<ul>
+					<li><a href="#">bedia günaydın</a></li>
+					<li><a href="#">cihan öncü</a></li>
+					<li><a href="#">deniz özgün</a></li>
+					<li><a href="#">ergin turunç</a></li>
+					<li><a href="#">mustafa çetin</a></li>
+					<li><a href="#">mustafa nurdogdu</a></li>
+					<li><a href="#">ozan kiymaç</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </div>
